dc7694a50ac18f079517cad8ed4d3d3d9b442858
OhSeungSe <39735663+amdradeon@users.noreply.github.com>
2018-05-29 23:13:42 +0900

    randomnumber함수추가

--- TodayNumber.c
+++ TodayNumber.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+int randomnumber();
+
 int main(){
 
 	srand((unsigned int)time(0));
 
+	while(1){
+		printf("오늘의 로또번호를 출력하시겠습니까?\n");
 
+
+	}
 
 
 
 
 	return 0;}
+
+int randomnumber(){
+
+	return rand()%45+1;
+}
